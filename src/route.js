eaebb249475ed9eb382558dc1d81c4f236de40f1
SilverioSimoes <silverio.simoes@gmail.com>
2021-07-10 16:06:58 -0300

    main

--- src/route.js
+++ src/route.js
@@ -6,4 +6,7 @@ route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
 
+// Formato do formulário da modal
+// route.post('/room/:room/:question/:action', (req, res) => res.render())
+
 module.exports = route
