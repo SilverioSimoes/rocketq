7334cd9d01fa7ec3ae9c7dc1f99183d36d14e643
SilverioSimoes <silverio.simoes@gmail.com>
2021-07-13 22:17:35 -0300

    passincorrect

--- src/route.js
+++ src/route.js
@@ -7,10 +7,9 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", { page: 'enter-room' }))
 route.get('/create-pass', (req, res) => res.render("index", { page: 'create-pass' }))
 
-// Formato do formulário da modal
 route.post('/create-room', RoomController.create)
 route.get('/room/:room', RoomController.open)
-route.get('/enterroom', RoomController.enter)
+route.post('/enterroom', RoomController.enter)
 
 route.post('/question/create/:room', QuestionController.create)
 route.post('/question/:room/:question/:action', QuestionController.index)
--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ const server = express()
 
 server.set('view engine', 'ejs')
 
+server.use(express.static("public"))
+
 server.set('views', path.join(__dirname, 'views'))
 
 server.use(express.urlencoded({ extended: true }))
 
 server.use(route)
-
-server.use(express.static('public'))
 
 server.listen(3000, () => console.log("RODANDO"))
